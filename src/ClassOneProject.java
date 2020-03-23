f67f4990fcc18ed1a1d849d98966a613229ddb3f
sameer <sam.shivane3362@gmail.com>
2020-03-23 13:16:23 +0530

    project commited

---
+++ src/ClassOneProject.java
@@ -0,0 +1,19 @@
+
+public class ClassOneProject {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main Method executed");
+		method();
+		method2();
+	}
+
+	public static void method() {
+		System.out.println("Method One");
+	}
+
+	public static void method2() {
+		System.out.println("Method Two");
+	}
+
+}
